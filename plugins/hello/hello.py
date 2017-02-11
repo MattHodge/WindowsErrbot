781aa1d31ff293de733cab7ac2372e3709763928
Matthew Hodgkins <matthodge@gmail.com>
2017-02-11 16:53:43 +0100

    add bot_arg cmds

--- plugins/hello/hello.py
+++ plugins/hello/hello.py
@@ -1,4 +1,4 @@
-from errbot import BotPlugin, botcmd
+from errbot import BotPlugin, botcmd, arg_botcmd
 
 class hello(BotPlugin):
     """Example 'Hello, world!' plugin for Errbot"""
@@ -6,7 +6,7 @@ class hello(BotPlugin):
     @botcmd
     def hello(self, msg, args):
         """Say hello to the world"""
-        return "Hello, world!"
+        yield "Hello, world!"
 
     @botcmd
     def hellopretty(self, msg, args):
@@ -25,3 +25,24 @@ Write-Output "In Germany!"
 """
         yield code_block
         yield "https://api.giphy.com/img/giphy_translate.gif"
+
+    @arg_botcmd('name', type=str)
+    def saymyname(self, message, name=None):
+        """Say the entered name"""
+        yield "Hello, {first_name}".format(
+            first_name=name
+        )
+
+    @arg_botcmd('name', type=str)
+    @arg_botcmd('--last-name', dest='last_name', type=str)
+    def saymynameagain(self, message, name=None, last_name=None):
+        """Say the name and optional last name"""
+        if last_name:
+            yield "Hello again, {first_name} {last_name}".format(
+                first_name=name,
+                last_name=last_name,
+            )
+        else:
+            yield "Hello again, {first_name}".format(
+                first_name=name
+            )
