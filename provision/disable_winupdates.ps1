60a93f836ed22f1f30ded3a17e9ce9add689a1cc
Matthew Hodgkins <matthodge@gmail.com>
2017-04-13 22:44:46 +0200

    enable rdp

--- provision/disable_winupdates.ps1
+++ provision/disable_winupdates.ps1
@@ -3,3 +3,8 @@ $WUSettings.NotificationLevel = 1
 $WUSettings.save()
 
 Set-Item WSMan:\\localhost\\Client\\TrustedHosts * -Force
+
+# Enable RDP
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1
